12e9f37a8e4681ab6734f2a1972bda7ec2ec8739
Héctor Gonzalo Andrés <hectorgoan@gmail.com>
2021-12-20 14:24:07 +0100

    Theming

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ const app = Vue.createApp({
     data() {
         return {
             todayDate: "",
+            todayDateTime: "",
         };
     },
     created() {
         const timeElapsed = Date.now();
         const today = new Date(timeElapsed);
         this.todayDate = today.toLocaleDateString();
+        this.todayDateTime = today.toLocaleTimeString();
     },
     methods: {},
 });
